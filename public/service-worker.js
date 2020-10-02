912d489cff03ab99ceb7da4559b3ed752f08c0cc
Oscar Chiriboga <oscarmchiriboga@gmail.com>
2020-10-02 10:40:57 -0400

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,79 +1,69 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/icons/icon-512x512.png',
-    '/icons/icon-192x192.png',
-    '/index.js',
-    '/manifest.json',
-    '/db.js'
-  ];
-  
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith("/api/")) {
-      event.respondWith(
-        caches.match(RUNTIME).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icons/icon-512x512.png',
+  '/icons/icon-192x192.png',
+  '/index.js',
+  '/manifest.json',
+  '/db.js'
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
           }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
         })
       );
-    }
+    })
+  );
 
+  self.clients.claim();
+});
 
-    // event.respondWith(fetch(event.request).catch(function(){
-    //   return caches.match(event.request).then(function(response){
-    //     if(response){
-    //       return response;
-    //     }else if(event.request.headers.get("accept).includes("text/html")){
-      //return cached for the homepage for all requests of html pages
-    //       return caches.match("/");
-    //     })
-    //     }
-    //   })
-    // }))
+// fetch
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
 
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
 
-  });
+    return;
+  }
+});
 
- 
-  
