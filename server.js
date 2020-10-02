740675670197f923696feb0ed4a93da91f2cafb0
Oscar Chiriboga <oscarmchiriboga@gmail.com>
2020-10-02 10:08:01 -0400

    updated port

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(mainDir, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
